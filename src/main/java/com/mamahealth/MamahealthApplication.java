2cad9b179ccfab990437aefaa35fa06a1a4c5296
chamayma <chamayma22@gmail.com>
2026-07-05 10:11:40 +0300

    initial push

---
+++ src/main/java/com/mamahealth/MamahealthApplication.java
@@ -0,0 +1,13 @@
+package com.mamahealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamahealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MamahealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mamahealth/MamahealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamahealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MamahealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
